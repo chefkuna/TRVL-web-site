2c193a6151ed5081e3f728a3bcbcdd9879d13d9d
chefkuna <sukhyun94@naver.com>
2022-08-28 22:25:44 +0900

    Herosection components

---
+++ .history/src/components/HeroSection_20220828222251.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Button } from './Button';
+import './Herosection.css';
+
+function HeroSection() {
+  return (
+    <div className='hero-container'>
+      <video src="../../public/videos/video-2.mp4" autoPlay loop muted/>
+      <h1>ADVENTURE AWAITS</h1>
+      <p>What are you waiting for?</p>
+      <div className="hero-btns">
+        <Button 
+          className='btns' 
+          buttonStyles='btn--outline' 
+          buttonSize='btn--large'
+        >
+          GET STARTED
+        </Button>
+      </div>
+    </div>
+  )
+}
+
+export default HeroSection;
