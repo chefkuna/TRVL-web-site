bfacfcbd27d0a2e734e80fe1b519f00bb12aedca
chefkuna <sukhyun94@naver.com>
2022-08-30 00:07:59 +0900

    Add Footer

---
+++ .history/src/components/Footer_20220829233257.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import { Button } from './Button';
+import './Footer.css';
+
+function Footer() {
+  return (
+    <div className='footer-container'>
+      <section className="footer-subscription">
+        <p className="footer-subscription-heading">
+          Join the Adeventure newletter to receive our best vacation deals
+        </p>
+        <p className="footer-subscription-text">
+          You can unsubscribe at any time.
+        </p>
+        <div className="input-areas">
+          <form>
+            <input type="email" name="email" placeholder='Your Email'
+            className="footer-input" />
+            <Button buttonStyle='btn--outline'>Subscribe</Button>
+          </form>
+        </div>
+      </section>
+      <div className="footer-links">
+        <div className="footer-link-wrapper">
+          <div className="footer-link-items"></div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
